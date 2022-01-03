00420def46c15685cf9a96190b879d96fc72b754
longg99 <vuhoanglong1512@gmail.com>
2022-01-03 17:05:21 -0500

    added backend with express

---
+++ index.js
@@ -0,0 +1,72 @@
+// backend to call the API safely
+// use express (nodejs web framework), cors (allows making request from one website to another)
+// dotenv (loads var from .env) and axios
+
+// listen on port 8000
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+// read from .env file
+require("dotenv").config({ path: "./.env" });
+
+const app = express();
+// use cors
+app.use(cors());
+
+app.get("/", (request, response, next) => {
+  response.json("Hello world!");
+});
+
+app.get("/states/:country", (request, response) => {
+  const country = request.params.country;
+  // custom options for CountryStateCity API
+  const options = {
+    headers: {
+      // the API key
+      "X-CSCAPI-KEY": process.env.REACT_APP_COUNTRY_API_KEY,
+    },
+    redirect: "follow",
+  };
+  // pass the option param
+  axios
+    .get(
+      `https://api.countrystatecity.in/v1/countries/${country}/states`,
+      options
+    )
+    .then((res) => {
+      // to json
+      response.json(res.data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+app.get("/cities/:country", (request, response) => {
+  const country = request.params.country;
+  // custom options for CountryStateCity API
+  const options = {
+    headers: {
+      // the API key
+      "X-CSCAPI-KEY": process.env.REACT_APP_COUNTRY_API_KEY,
+    },
+    redirect: "follow",
+  };
+  // pass the option param
+  axios
+    .get(
+      `https://api.countrystatecity.in/v1/countries/${country}/cities`,
+      options
+    )
+    .then((res) => {
+      // to json
+      response.json(res.data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// listen to the changes
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/Components/apis/index.js
+++ src/Components/apis/index.js
@@ -1,35 +1,20 @@
 import axios from "axios";
 
-// custom options for CountryStateCity API
-const options = {
-  headers: {
-    // the API key
-    "X-CSCAPI-KEY": process.env.REACT_APP_COUNTRY_API_KEY,
-  },
-  redirect: "follow",
-};
-
 // get all countries using RestCountries API
 // use v2 for sorted results
 export const getAllCountries = async () =>
   await axios.get("https://restcountries.com/v2/all");
 
-// get all states, given a country using CountryStateCity API
+// get all states, given a country using our backend
 export const getAllStates = async (country) =>
   // pass the option param
-  await axios.get(
-    `https://api.countrystatecity.in/v1/countries/${country}/states`,
-    options
-  );
+  await axios.get(`http://localhost:8000/states/${country}`);
 
 // get the information of a given country
 export const getCountryInfo = async (country) =>
   await axios.get(`https://restcountries.com/v3.1/alpha/${country}`);
 
-// get all cities
+// get all cities from our backend
 export const getAllCities = async (country) =>
   // pass the option param
-  await axios.get(
-    `https://api.countrystatecity.in/v1/countries/${country}/cities`,
-    options
-  );
+  await axios.get(`http://localhost:8000/cities/${country}`);
